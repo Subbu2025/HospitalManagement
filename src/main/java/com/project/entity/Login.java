5976d47ffdc2bc6755fbe9ddf67177bf865fcb1e
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-08-10 16:14:05 +0530

    Update Login.java

--- src/main/java/com/project/entity/Login.java
+++ src/main/java/com/project/entity/Login.java
@@ -2,43 +2,69 @@ package com.project.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 
 @Entity
-public class Login 
-{
-	private String id;	//foreign key, consists eid and pid
-	private String role;
-	@Id
-	private String username;	//primary key
-	private String password;
-	
-	Login(){}
-	
-	public Login(String id, String role, String username, String password) 
-	{
-		super();
-		this.id = id;
-		this.role=role;
-		this.username = username;
-		this.password = password;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public String getRole() {
-		return role;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public String getPassword() {
-		return password;
-	}
+public class Login {
 
-	@Override
-	public String toString() {
-		return "Login [id=" + id + ", role=" + role + ", username=" + username + ", password=" + password + "]";
-	}
-		
+    @Id
+    private String username; // primary key
+
+    private String password;
+
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private SomeOtherEntity id; // foreign key relationship
+
+    // Default constructor
+    public Login() {}
+
+    // Parameterized constructor
+    public Login(String username, String password, String role, SomeOtherEntity id) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public SomeOtherEntity getId() {
+        return id;
+    }
+
+    public void setId(SomeOtherEntity id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Login [username=" + username + ", password=" + password + ", role=" + role + ", id=" + id + "]";
+    }
 }
